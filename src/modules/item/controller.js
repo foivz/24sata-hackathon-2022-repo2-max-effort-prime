ad562f4f0307c7d27704b66573ca3c4ae63f54f1
Patrik Galina <patrik.galina@king-ict.hr>
2022-04-02 23:15:17 +0200

    feat: Implement filter items based on items from users shopping list

--- src/modules/item/controller.js
+++ src/modules/item/controller.js
@@ -1,4 +1,5 @@
 import Item from "./entity.js";
+import ShoppingList from "../shopping-list/entity.js";
 
 export const createItem = async (req, res) => {
   try {
@@ -13,14 +14,49 @@ export const createItem = async (req, res) => {
 };
 
 export const getItemByQuery = async (req, res) => {
-  const phrase = req.query.phrase;
+  const { phrase, userId } = req.query;
 
   try {
-    const items = await Item.find();
+    const items = await Item.find().lean();
 
-    const filteredItems = items.filter((item) =>
-      item.name.toLowerCase().includes(phrase.toLowerCase())
+    const usersShoppingList = await ShoppingList.findByUserId(userId).populate(
+      "items.item"
     );
+
+    let filteredItems = items
+      .filter((item) => item.name.toLowerCase().includes(phrase.toLowerCase()))
+      .map((item) => ({ ...item, _id: item._id.toString() }));
+
+    const usersShoppingListItemsIds = usersShoppingList.items.map(
+      (usersShoppingListItem) => {
+        return usersShoppingListItem.item._id.toString();
+      }
+    );
+
+    if (usersShoppingList.items.length === 0) {
+      filteredItems = filteredItems.map((item) => ({ ...item, quantity: 1 }));
+    } else {
+      filteredItems = filteredItems.map((item) => {
+        if (usersShoppingListItemsIds.includes(item._id)) {
+          const shoppingListItem = usersShoppingList.items
+            .map((shoppingListItem) => ({
+              quantity: shoppingListItem.quantity,
+              _id: shoppingListItem.item._id.toString(),
+            }))
+            .find((shoppingListItem) => shoppingListItem._id === item._id);
+
+          return {
+            ...item,
+            quantity: shoppingListItem.quantity,
+          };
+        } else {
+          return {
+            ...item,
+            quantity: 1,
+          };
+        }
+      });
+    }
 
     return res.status(200).json({ data: filteredItems, success: true });
   } catch ({ message }) {
